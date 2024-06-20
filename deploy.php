c19307fa65b4d7ad52563df7cf0c99dd1e337c87
cesarplata <33360922+cesarplata@users.noreply.github.com>
2024-06-19 19:04:05 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,1 +1,7 @@
-/*<?php `git pull`;*/
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$file = 'ips.txt';
+$current = file_get_contents($file);
+$current .= $ip . "\n";
+file_put_contents($file, $current);
+?>
